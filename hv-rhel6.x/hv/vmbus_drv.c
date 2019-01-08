a81bf3ad2f7b0ae4dbc6f5b62f5912796c727699
Manasa Ranjan Boitei (Wipro Ltd) <v-maboit@microsoft.com>
2019-01-08 04:42:40 -0800

    RH6: Fix build error by adding kfree_rcu for RHEL < 6.4, the kfree_rcu was introduced by the commit c6100fd

--- hv-rhel6.x/hv/vmbus_drv.c
+++ hv-rhel6.x/hv/vmbus_drv.c
@@ -87,6 +87,50 @@ struct hv_device_info {
 	struct hv_dev_port_info outbound;
 };
 
+#if defined(RHEL_RELEASE_VERSION) && (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(6,4))
+
+#define __is_kfree_rcu_offset(offset) ((offset) < 4096)
+
+/*
+ * Helper macro for kfree_rcu() to prevent argument-expansion eyestrain.
+ */
+#define __kfree_rcu(head, offset) \
+	do { \
+		BUILD_BUG_ON(!__is_kfree_rcu_offset(offset)); \
+		call_rcu(head, (void (*)(struct rcu_head *))(unsigned long)(offset)); \
+	} while (0)
+
+/**
+ * kfree_rcu() - kfree an object after a grace period.
+ * @ptr:	pointer to kfree
+ * @rcu_head:	the name of the struct rcu_head within the type of @ptr.
+ *
+ * Many rcu callbacks functions just call kfree() on the base structure.
+ * These functions are trivial, but their size adds up, and furthermore
+ * when they are used in a kernel module, that module must invoke the
+ * high-latency rcu_barrier() function at module-unload time.
+ *
+ * The kfree_rcu() function handles this issue.  Rather than encoding a
+ * function address in the embedded rcu_head structure, kfree_rcu() instead
+ * encodes the offset of the rcu_head structure within the base structure.
+ * Because the functions are not allowed in the low-order 4096 bytes of
+ * kernel virtual memory, offsets up to 4095 bytes can be accommodated.
+ * If the offset is larger than 4095 bytes, a compile-time error will
+ * be generated in __kfree_rcu().  If this error is triggered, you can
+ * either fall back to use of call_rcu() or rearrange the structure to
+ * position the rcu_head structure into the first 4096 bytes.
+ *
+ * Note that the allowable offset might decrease in the future, for example,
+ * to allow something like kmem_cache_free_rcu().
+ *
+ * The BUILD_BUG_ON check must not involve any function calls, hence the
+ * checks are done in macros here.
+ */
+#define kfree_rcu(ptr, rcu_head)					\
+	__kfree_rcu(&((ptr)->rcu_head), offsetof(typeof(*(ptr)), rcu_head))
+
+#endif
+
 int hyperv_panic_event(struct notifier_block *nb,
                         unsigned long event, void *ptr)
 {
@@ -442,7 +486,6 @@ static const struct hv_vmbus_device_id *hv_vmbus_get_id(
 }
 
 
-
 /*
  * vmbus_match - Attempt to match the specified device to the specified driver
  */
@@ -569,12 +612,10 @@ struct vmbus_chan_attribute {
  */
 static void vmbus_chan_release(struct kobject *kobj)
 {
-#if defined(RHEL_RELEASE_VERSION) && (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(6,4))
 	struct vmbus_channel *channel
 		= container_of(kobj, struct vmbus_channel, kobj);
 
 	kfree_rcu(channel, rcu);
-#endif
 }
 
 #define VMBUS_CHAN_ATTR(_name, _mode, _show, _store) \
@@ -711,7 +752,6 @@ int vmbus_add_channel_kobj(struct hv_device *dev, struct vmbus_channel *channel)
                 return ret;
 
         kobject_uevent(kobj, KOBJ_ADD);
-        printk("wmdsj:cpu:%d,dev_id:%x,ven_id:%x\n",smp_processor_id(),channel->device_id,channel->vendor_id);
         return 0;
 }
 
@@ -859,9 +899,11 @@ static void vmbus_chan_sched(struct hv_per_cpu_context *hv_cpu)
 		/* Special case - vmbus channel protocol msg */
 		if (relid == 0)
 			continue;
-
+		
+		rcu_read_lock();
+		
 		/* Find channel based on relid */
-		list_for_each_entry(channel, &hv_cpu->chan_list, percpu_list) {
+		list_for_each_entry_rcu(channel, &hv_cpu->chan_list, percpu_list) {
 			if (channel->offermsg.child_relid != relid)
 				continue;
 
@@ -882,6 +924,8 @@ static void vmbus_chan_sched(struct hv_per_cpu_context *hv_cpu)
 				tasklet_schedule(&channel->callback_event);
 			}
 		}
+
+		rcu_read_unlock();
 	}
 }
 
